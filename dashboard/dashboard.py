da90180258f03dc98b75fa53b5f66bcda0022772
Bagus Satrio Wicaksono <118421226+ssugab@users.noreply.github.com>
2024-02-03 22:26:44 +0700

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,7 +5,7 @@ import seaborn as sns
 sns.set(style='dark')
 
 # Load data
-bikeSharing_df = pd.read_csv("all_data.csv") 
+bikeSharing_df = pd.read_csv("Bike-Sharing-EDA/dashboard/all_data.csv") 
 
 # Mapping label cuaca
 weather_cond = {
